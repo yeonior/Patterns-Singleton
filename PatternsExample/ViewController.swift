19a5f15aeaf85ad8d905e227f9eed9032be2383c
Rus <de.mr44@gmail.com>
2021-12-30 23:17:17 +0500

    Initial Commit

---
+++ PatternsExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PatternsExample
+//
+//  Created by Ruslan on 30.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
